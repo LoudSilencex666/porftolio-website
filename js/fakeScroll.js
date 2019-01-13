023b5a3623cbf774528e0594c48f762e965e76c1
Damian Giedrojć <icelookpl@gmail.com>
2019-01-13 02:59:36 +0100

    General concept, and first fake scroll attempt

---
+++ js/fakeScroll.js
@@ -0,0 +1,53 @@
+function fakeScroll() {
+    let isAnimating = false;
+    let scrollPosition = 0;
+    let activeSection = 0;
+    window.addEventListener('wheel', function(evt) {
+        
+        if (!isAnimating) {
+            isAnimating = true;
+
+            let turn;
+            
+            if (evt.deltaY > 0) {
+                turn = 1;
+            } else if (evt.deltaY < 0) {
+                turn = -1;
+            }
+
+            console.log(turn);
+
+            
+
+            if (scrollPosition == 0 && turn == -1) {
+                scrollPosition = scrollPosition;
+                activeSection = activeSection;
+            } else if ( scrollPosition == 87.5 && turn == 1) {
+                scrollPosition = scrollPosition;
+                activeSection = activeSection;
+            } else {
+                scrollPosition += 12.5 * turn;
+                activeSection += 1 * turn;
+            }
+
+            moveFakeScroll(scrollPosition);
+            changeSection(activeSection);
+            
+            setTimeout(function() {
+                isAnimating = false;
+            }, 100);
+        }
+
+    });
+}
+
+function moveFakeScroll(scrollPosition) {
+    const element = document.querySelector('.fake-scroll');
+    element.style.top = `${scrollPosition}%`;
+}
+
+function changeSection(activeSection) {
+    const element = document.querySelector('.content-container');
+    activeSection = -activeSection;
+    element.style.left = `${activeSection * 100}%`;
+}
