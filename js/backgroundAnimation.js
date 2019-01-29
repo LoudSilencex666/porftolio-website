b8d870f2573e5d123d5253278504a8faef7eb493
Damian Giedrojć <icelookpl@gmail.com>
2019-01-29 16:51:50 +0100

    background-animation concept 2

--- js/backgroundAnimation.js
+++ js/backgroundAnimation.js
@@ -1,9 +1,9 @@
 const canvasBA = {
     eleDOM : window.document.querySelector('#backgroundAnimation'),
-    
+
     mx : null,
     my : null,
-    
+
     get rect() {
         return this.eleDOM.getBoundingClientRect();
     },
@@ -12,7 +12,7 @@ const canvasBA = {
     },
     get h() {
         return this.eleDOM.height;
-    }, 
+    },
     get ctx() {
         return this.eleDOM.getContext('2d');
     }
@@ -24,10 +24,10 @@ let tile = new Image();
 let tileLoaded = false;
 // let tileW = 159;
 // let tileH = 137;
-let tileW = 159;
-let tileH = 137;
+let tileW = 95;
+let tileH = 82;
 tile.onload = () => { tileLoaded = true;};
-tile.src = './assets/background_animation/tile3.png';
+tile.src = './assets/background_animation/tile4.png';
 
 function getMousePos(evt) {
     return {
@@ -59,34 +59,37 @@ function canvasMouseEffect() {
     canvasBA.ctx.arc(canvasBA.mx, canvasBA.my, 200, 0, 2 * Math.PI, false);
     // create radial gradient
     var grd = canvasBA.ctx.createRadialGradient(canvasBA.mx, canvasBA.my, 10, canvasBA.mx, canvasBA.my, 200);
-    
-    grd.addColorStop(0, 'rgba(8, 146, 208, 0.2)');
 
-    grd.addColorStop(1, 'rgba(22, 22, 22, 0.2)');
-    
+    grd.addColorStop(0, 'rgba(8, 146, 208, 1)');
+
+    grd.addColorStop(1, 'rgba(22, 22, 22, 1)');
+
     canvasBA.ctx.fillStyle = grd;
 
     // canvasBA.ctx.fillStyle = 'rgba(0, 0, 111, 1)';
 
     canvasBA.ctx.fill();
-    
+
 }
 
 function canvasTiles() {
     if(tileLoaded) {
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 10; j++) {
+        for (let i = 0; i < 20; i++) {
+            for (let j = 0; j < 20; j++) {
                 canvasBA.ctx.drawImage(tile, -tileW / 2 + (1.5*j * tileW), -tileH / 2 + (i * tileH));
             }
         }
 
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 10; j++) {
+        for (let i = 0; i < 20; i++) {
+            for (let j = 0; j < 20; j++) {
                 canvasBA.ctx.drawImage(tile, tileW / 4 + (1.5*j * tileW), 0 + (i * tileH));
             }
         }
-        
+
     }
+    // if(tileLoaded) {
+    //     canvasBA.ctx.drawImage(tile, 0, 0, canvasBA.w, canvasBA.h);
+    // }
 }
 
 
